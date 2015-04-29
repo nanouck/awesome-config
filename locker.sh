e05770f0459d8d993427fa15c25ceeee1b7fe796
Nicolas Dely <nicolas.dely@technicolor.com>
2015-04-29 16:02:52 +0200

    fix xautolock parameters

--- locker.sh
+++ locker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec xautolock -detectsleep 
+exec xautolock -detectsleep \
   -time 15 -locker "gnome-screensaver-command --lock" \
   -notify 30 \
   -notifier "notify-send -u normal -t 10000 -- 'LOCKING screen in 30 seconds'"
